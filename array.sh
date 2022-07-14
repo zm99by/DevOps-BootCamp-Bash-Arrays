0b180ea455141144295716688d43017b9a2263e8
Vladimir Sakhonchik <Vladimir_Sakhonchik@epam.com>
2022-07-14 22:24:28 +0200

    add last hot fix

--- array.sh
+++ array.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-arr=(a b c d e h g f i j k l m n o p q r s t u v w x y z)
+arr=( {a..z} )
 for (( i = 0 ; i < $1 ; i++ )); do 
-    mkdir folder_${arr[i]}
+   mkcir folder_${arr[i]}
 done
